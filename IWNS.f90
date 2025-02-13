5001c559c9ea151be181df9d89eba08fbcee7c7d
lizl2000 <3256890579@qq.com>
2025-02-13 09:42:25 +0800

    cold parallel wave dipserison curve

--- IWNS.f90
+++ IWNS.f90
@@ -41,46 +41,47 @@
         end if
 		ti_div_te=1.0_wp
         c_div_v_para_input=470000.0_wp
-		k_para_c_div_omega_ci=7.0_wp
-		omega_pe_div_omega_ce_input=1.0_wp
-		k_para_rho_i_para_input=k_para_c_div_omega_ci/(c_div_v_para_input)**(0.5)
-		k_para_rho_e_para_input=k_para_rho_i_para_input*(1.0_wp/1836.0_wp/ti_div_te)**(0.5)
-		k_para_rho_e_per_input=k_para_rho_e_para_input
-		k_per_rho_i_para_input=0.0_wp
-		k_per_rho_i_per_input=0.0_wp
-		k_per_rho_e_para_input=0.0_wp
-		k_per_rho_e_per_input=0.0_wp
-		call set_parameter(c_div_v_para_input,omega_pe_div_omega_ce_input,k_para_rho_i_para_input,k_para_rho_e_para_input,k_para_rho_e_per_input,k_per_rho_i_para_input,k_per_rho_i_per_input,k_per_rho_e_para_input,k_per_rho_e_per_input)
+		do k=1,30
+			k_para_c_div_omega_ci=50.0_wp*k
+			omega_pe_div_omega_ce_input=1.0_wp
+			k_para_rho_i_para_input=k_para_c_div_omega_ci/(c_div_v_para_input)**(0.5)
+			k_para_rho_e_para_input=k_para_rho_i_para_input*(1.0_wp/1836.0_wp/ti_div_te)**(0.5)
+			k_para_rho_e_per_input=k_para_rho_e_para_input
+			k_per_rho_i_para_input=0.0_wp
+			k_per_rho_i_per_input=0.0_wp
+			k_per_rho_e_para_input=0.0_wp
+			k_per_rho_e_per_input=0.0_wp
+			call set_parameter(c_div_v_para_input,omega_pe_div_omega_ce_input,k_para_rho_i_para_input,k_para_rho_e_para_input,k_para_rho_e_per_input,k_per_rho_i_para_input,k_per_rho_i_per_input,k_per_rho_e_para_input,k_per_rho_e_per_input)
 
-		left_edge=0.01_wp
-		right_edge=1.51_wp
-		down_edge=-4*k_para_rho_i_para_input
-		up_edge=0.21_wp 
+			left_edge=0.01_wp
+			right_edge=5.01_wp
+			down_edge=-0.5_wp
+			up_edge=0.21_wp 
 
-		allocate(ans_z_solve(n_error))
-		allocate(ans_mul_solve(n_error))
-		allocate(ans_z_error(n_error))
-		allocate(ans_f_solve(n_error))
+			allocate(ans_z_solve(n_error))
+			allocate(ans_mul_solve(n_error))
+			allocate(ans_z_error(n_error))
+			allocate(ans_f_solve(n_error))
 
-		call zero_pole_location(cold_plasma_dispersion_function,ierr,left_edge,right_edge,down_edge,up_edge,kc_square,epsilon_i,epsilon_accuracy_limit,n_circle,n_line,epsilon_0,z_solve_number,ans_z_solve,ans_mul_solve,ans_z_error,ans_f_solve)
-		
-		if (my_id==0) then
-			do n=1,z_solve_number
-				write(*,*),n,':'
-				write(*,*),'ans_z_solve are',ans_z_solve(n)
-				write(*,*),'ans_mul_solve are',ans_mul_solve(n)
-				write(*,*),'ans_z_error are',ans_z_error(n)
-				write(*,*),'ans_f_solve are',ans_f_solve(n)
-				call polarization(ans_z_solve(n),eigen,polar)
-				write(fid,'(*(G30.7,:,",",X))') k_para_c_div_omega_ci,n,real(ans_z_solve(n)),aimag(ans_z_solve(n)),ans_mul_solve(n),ans_z_error(n),abs(ans_f_solve(n)),eigen,polar(1),polar(2),polar(3)
-				
-			end do
-		end if
-		deallocate(ans_z_solve)
-		deallocate(ans_mul_solve)
-		deallocate(ans_z_error)
-		deallocate(ans_f_solve)
-        
+			call zero_pole_location(cold_plasma_dispersion_function,ierr,left_edge,right_edge,down_edge,up_edge,kc_square,epsilon_i,epsilon_accuracy_limit,n_circle,n_line,epsilon_0,z_solve_number,ans_z_solve,ans_mul_solve,ans_z_error,ans_f_solve)
+			
+			if (my_id==0) then
+				do n=1,z_solve_number
+					write(*,*),n,':'
+					write(*,*),'ans_z_solve are',ans_z_solve(n)
+					write(*,*),'ans_mul_solve are',ans_mul_solve(n)
+					write(*,*),'ans_z_error are',ans_z_error(n)
+					write(*,*),'ans_f_solve are',ans_f_solve(n)
+					call polarization(ans_z_solve(n),eigen,polar)
+					write(fid,'(*(G30.7,:,",",X))') k_para_c_div_omega_ci,n,real(ans_z_solve(n)),aimag(ans_z_solve(n)),ans_mul_solve(n),ans_z_error(n),abs(ans_f_solve(n)),eigen,polar(1),polar(2),polar(3)
+					
+				end do
+			end if
+			deallocate(ans_z_solve)
+			deallocate(ans_mul_solve)
+			deallocate(ans_z_error)
+			deallocate(ans_f_solve)
+		end do
         call cpu_time(finish_cpu_time)
         if (my_id==0) then
 			close(fid)
