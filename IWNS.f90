4326907af7eaa29177fa3d96eba3a59e6061c9d2
lizl2000 <3256890579@qq.com>
2025-03-20 10:54:08 +0800

    A version for calculating Bernstein wave boundary

--- IWNS.f90
+++ IWNS.f90
@@ -20,7 +20,7 @@
         real(wp)::start_cpu_time,finish_cpu_time
         real(wp)::eigen
         complex(wp)::polar(3)
-		real(wp),allocatable::right_wave_cma_x(:),right_wave_cma_y(:)
+		real(wp),allocatable::x_wave_cma_x(:),x_wave_cma_y(:)
 		integer::direction,kmax,ti_number
 		real(wp)::ti
 		real(wp)::wave_max_imag, wave_max_real, wave_max_real_last, least_damped_ratio
@@ -38,64 +38,65 @@
 		n_circle=400
 		n_line=400
 		epsilon_0=0.1_wp
-        kmax=90
+        kmax=38
         if (my_id==0) then
-	  		open(fid_process,file='output_in_process.csv')
-			open(fid, file='omega_ce_cma.csv')
+	  		open(fid_process,file='x_wave_boundary_output_in_process.csv')
+			open(fid, file='x_wave_boundary.csv')
         end if
 		ti_div_te=1.0_wp
-		k_per_rho_i_para_input=0.0_wp
-		k_per_rho_i_per_input=0.0_wp
-		k_per_rho_e_para_input=0.0_wp
-		k_per_rho_e_per_input=0.0_wp
-		do ti_number=3,10
+		k_para_rho_i_para_input=0.0_wp
+		k_para_rho_e_para_input=0.0_wp
+		k_para_rho_e_per_input=0.0_wp
+		
+		do ti_number=1,1
 			ti=ti_number*0.1_wp
 			c_div_v_para_input=470000.0_wp/(0.1_wp)/ti_number
-			allocate(right_wave_cma_x(110))
-			allocate(right_wave_cma_y(110))
+			allocate(x_wave_cma_x(110))
+			allocate(x_wave_cma_y(110))
 			do k=1,kmax
-				omega_pe_div_omega_ce_input=10**(-1.5_wp+0.05_wp*k)
-				direction=2
-				k_para_rho_i_para_input=0.0_wp
-
+				omega_pe_div_omega_ce_input=0.1_wp*k
+				direction=1
+				k_per_rho_i_para_input=0.0_wp
+				wave_max_real_last=0.0_wp
 				do while (direction/=0)
-					if (direction==2) then
-						k_para_rho_i_para_input=k_para_rho_i_para_input+1.0_wp
+					
+					if (direction==1) then
+						k_per_rho_i_para_input=k_per_rho_i_para_input+1.0_wp
 					else if (direction==-1) then
-						k_para_rho_i_para_input=k_para_rho_i_para_input-0.1_wp
-					else if (direction==1) then
-						k_para_rho_i_para_input=k_para_rho_i_para_input+0.01_wp
+						k_per_rho_i_para_input=k_per_rho_i_para_input-0.1_wp
 					end if 
-					if (k_para_rho_i_para_input<=0) then
+					if (k_per_rho_i_para_input<=0) then
 						wave_max_real=1.0_wp
 						exit
 					end if
-					k_para_rho_e_para_input=-k_para_rho_i_para_input*(1.0_wp/1836.0_wp/ti_div_te)**(0.5)
-					k_para_rho_e_per_input=k_para_rho_e_para_input
+					k_per_rho_i_per_input=k_per_rho_i_para_input
+					k_per_rho_e_para_input=-k_per_rho_i_para_input*(1.0_wp/1836.0_wp/ti_div_te)**(0.5)
+					k_per_rho_e_per_input=k_per_rho_e_para_input
 					
 					call set_parameter(c_div_v_para_input,omega_pe_div_omega_ce_input,k_para_rho_i_para_input,k_para_rho_e_para_input,k_para_rho_e_per_input,k_per_rho_i_para_input,k_per_rho_i_per_input,k_per_rho_e_para_input,k_per_rho_e_per_input)
 
-					wave_max_real=1000.0_wp
-					wave_max_imag=-1000.0_wp
+					wave_max_real=0.0_wp
+					wave_max_imag=-0.0_wp
 					least_damped_ratio=100000.0_wp
-					do region_i=1,18
-						if (region_i==1) then
-							left_edge=0.1_wp
-							right_edge=100.0_wp
-							down_edge=max(-4*k_para_rho_i_para_input,-4.0_wp)
-							up_edge=4.0_wp 
-						else
-							left_edge=100.0_wp+100.0_wp*(region_i-2)
-							right_edge=left_edge+100.0_wp
-							down_edge=max(-20*k_para_rho_i_para_input,-0.1*left_edge)
-							up_edge=4.0_wp 
+					left_edge=1800.0_wp
+					right_edge=1850.0_wp
+					up_edge=0.6_wp 
+					down_edge=-0.4_wp
+					do while (right_edge<(omega_pe_div_omega_ce_input+1)**0.5*1836)
+						left_edge=left_edge+50.0_wp
+						right_edge=right_edge+50.0_wp
+						if(right_edge>=(omega_pe_div_omega_ce_input+1)**0.5*1836) then
+							right_edge=(omega_pe_div_omega_ce_input+1)**0.5*1836
+						end if
+						if(right_edge>=3672.0_wp) then
+							exit
 						end if
 						allocate(ans_z_solve(n_error))
 						allocate(ans_mul_solve(n_error))
 						allocate(ans_z_error(n_error))
 						allocate(ans_f_solve(n_error))
 
-						call zero_pole_location(dispersion_function_parallel_1,ierr,left_edge,right_edge,down_edge,up_edge,kc_square,epsilon_i,epsilon_accuracy_limit,n_circle,n_line,epsilon_0,z_solve_number,ans_z_solve,ans_mul_solve,ans_z_error,ans_f_solve)
+						call zero_pole_location(dispersion_function_perpendicular,ierr,left_edge,right_edge,down_edge,up_edge,kc_square,epsilon_i,epsilon_accuracy_limit,n_circle,n_line,epsilon_0,z_solve_number,ans_z_solve,ans_mul_solve,ans_z_error,ans_f_solve)
 						do n=1,z_solve_number
 							if (abs(aimag(ans_z_solve(n)))<least_damped_ratio*real(ans_z_solve(n))) then
 								wave_max_imag=aimag(ans_z_solve(n))
@@ -109,28 +110,30 @@
 						deallocate(ans_f_solve)
 					end do
 					if (my_id==0) then
-						write(fid_process,'(*(G30.7,:,",",X))') omega_pe_div_omega_ce_input, direction,k_para_rho_i_para_input,wave_max_real,wave_max_imag
+						write(fid_process,'(*(G30.7,:,",",X))') omega_pe_div_omega_ce_input, direction,k_per_rho_i_para_input,wave_max_real,wave_max_imag
 					end if
-					if (direction*abs(wave_max_imag)>0.01*direction*abs(wave_max_real)) then
-						direction=-(direction+abs(direction)-2)/2
+					if(wave_max_real>0) then    
+						if (wave_max_real<=wave_max_real_last) then
+							direction=-(direction+abs(direction))/2
 					
-						if (direction==0) then
-							wave_max_real=(wave_max_real+wave_max_real_last)/2
-						end if 
-						
+							if (direction==0) then
+								wave_max_real=(wave_max_real+wave_max_real_last)/2
+							end if 
+					
+						end if
+	   
+						wave_max_real_last=wave_max_real
 					end if
-					wave_max_real_last=wave_max_real
 				end do
-				right_wave_cma_x(k)=1836**2*omega_pe_div_omega_ce_input/wave_max_real**2  
-				right_wave_cma_y(k)=1836.0_wp/wave_max_real
+				x_wave_cma_x(k)=1836**2*omega_pe_div_omega_ce_input/wave_max_real**2  
+				x_wave_cma_y(k)=1836.0_wp/wave_max_real
+				if (my_id==0) then
+					write(fid,'(*(G30.7,:,",",X))')  ti, x_wave_cma_x(k) ,x_wave_cma_y(k)					
+				end if
 			end do
-			if (my_id==0) then
-				do k=1,kmax
-					write(fid,'(*(G30.7,:,",",X))')  ti, right_wave_cma_x(k) ,right_wave_cma_y(k)
-				end do
-			end if
-			deallocate(right_wave_cma_x)
-			deallocate(right_wave_cma_y)
+			
+			deallocate(x_wave_cma_x)
+			deallocate(x_wave_cma_y)
 		end do
         call cpu_time(finish_cpu_time)
 		
